e08b817c1ceda483444209db227825af954f1c10
David Hedlund <david.v.hedlund@gmail.com>
2026-06-29 11:02:15 +0200

    fix(authority): prevent chromium bundling on vercel

--- next.config.js
+++ next.config.js
@@ -6,14 +6,26 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   experimental: {
     serverActions: {},
   },
-  serverExternalPackages: ["@sparticuz/chromium"],
+
+  serverExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push({
+        '@sparticuz/chromium': 'commonjs @sparticuz/chromium',
+        'puppeteer-core': 'commonjs puppeteer-core',
+      });
+    }
+
+    return config;
+  },
 };
 
-// Koppla next-intl till src/i18n/request.ts (viktigt!)
+// Koppla next-intl till src/i18n/request.ts
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 module.exports = withNextIntl(nextConfig);
-
