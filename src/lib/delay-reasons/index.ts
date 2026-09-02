dfe3e201120a782fc63ff2ccc4c026e2a62e8591
zamiocflight <david.v.hedlund@gmail.com>
2026-09-02 13:15:59 +0200

    feat(delay-reasons): export assessment engine

--- src/lib/delay-reasons/index.ts
+++ src/lib/delay-reasons/index.ts
@@ -1,2 +1,11 @@
 export { resolveDelayReason } from "./resolver";
 export { buildDelayReasonMetadata } from "./metadata";
+export {
+  getDelayReasonAssessment,
+  getAllDelayReasonAssessments,
+} from "./assessment";
+export type {
+  DelayReasonAssessmentProfile,
+  DelayReasonCategory,
+  LiabilityBaseline,
+} from "./assessment";
