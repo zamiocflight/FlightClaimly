1ddbe08909ccdd3cad0bc74887680646b2d69b60
zamiocflight <david.v.hedlund@gmail.com>
2026-09-02 18:13:39 +0200

    feat(passenger-rights): expose v1 knowledge model

---
+++ src/data/passenger-rights/index.ts
@@ -0,0 +1,15 @@
+export {
+  getAllPassengerRights,
+  getPassengerRight,
+  passengerRightsRegistry,
+} from "./registry";
+export { getAllLegalRules, getLegalRule, legalRulesRegistry } from "./rules";
+export type {
+  LegalBurden,
+  LegalOutcome,
+  LegalRule,
+  LegalRuleCondition,
+  PassengerRight,
+  PassengerRightCategory,
+  PassengerRightsAssessment,
+} from "./types";
