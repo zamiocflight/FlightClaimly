7084a5e0314071839c5989e649defb2dc0dd1f9f
zamiocflight <david.v.hedlund@gmail.com>
2026-09-04 14:23:11 +0200

    feat: export localization engine foundation

---
+++ src/lib/localization/index.ts
@@ -0,0 +1,24 @@
+export {
+  getLocaleDefinition,
+  interpolateLabel,
+  localeDefinitions,
+} from "./locales";
+export {
+  isLocalizationPublishable,
+  isLocalizationQualityComplete,
+  localizationRollout,
+  resolveLocalizationPublicationStatus,
+} from "./policy";
+export type {
+  KnowledgeLocalization,
+  LocalizableKnowledgeEntityType,
+  LocalizationPublicationStatus,
+  LocalizationQuality,
+  LocalizationResolution,
+  LocalizationSource,
+  LocalizedKnowledgeContent,
+  LocalizedKnowledgeLabels,
+  LocalizedSeoMetadata,
+  LocaleDefinition,
+  SeoLocale,
+} from "./types";
